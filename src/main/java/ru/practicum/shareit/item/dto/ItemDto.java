5b1847fdd827fde6b4ee3dceaed6639e4bdc20ef
Vladislav Surkov <vladucha09@inbox.ru>
2023-07-01 05:06:26 +0300

    feat: dto classes

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,22 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.*;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@Builder
 public class ItemDto {
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String description;
+    @NotNull
+    private Boolean available;
+    private Long itemRequestId;
 }

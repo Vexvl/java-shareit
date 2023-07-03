60c1e4f092a8a46d2e96254bf013a6bd96a9b4d5
Vladislav Surkov <vladucha09@inbox.ru>
2023-07-03 05:10:22 +0300

    feat: ISE + BR ExceptionsHandler

--- src/main/java/ru/practicum/shareit/item/exception/ExceptionsHandler.java
+++ src/main/java/ru/practicum/shareit/item/exception/ExceptionsHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.practicum.shareit.user.exception.InvalidRequestException;
 
 import java.util.Map;
 
@@ -19,5 +20,17 @@ public class ExceptionsHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, String> handleNotFound(AccessDeniedException e) {
         return Map.of("error", "Вещь не принадлежит user", "errorMessage", "Ошибка на сервере");
+    }
+
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleThrowable(Throwable t) {
+        return Map.of("error", "Возникло непредвиденное исключение", "errorMessage", "Ошибка на сервере");
+    }
+
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleThrowable(InvalidRequestException t) {
+        return Map.of("error", "BAD_REQUEST", "errorMessage", "Ошибка на сервере");
     }
 }

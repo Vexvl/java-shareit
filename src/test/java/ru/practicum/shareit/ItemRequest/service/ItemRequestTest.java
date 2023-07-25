38e953af81493505aff65e7d6e12e8189f5677c1
Vladislav Surkov <vladucha09@inbox.ru>
2023-07-25 22:53:21 +0300

    BookingIntegrationTests.java BookingRepositoryTest.java BookingUnitTests.java ItemRequestUnitTest.java ItemUnitTests.java

--- src/test/java/ru/practicum/shareit/ItemRequest/service/ItemRequestTest.java
+++ src/test/java/ru/practicum/shareit/ItemRequest/service/ItemRequestTest.java
@@ -17,7 +17,6 @@ import javax.persistence.TypedQuery;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 

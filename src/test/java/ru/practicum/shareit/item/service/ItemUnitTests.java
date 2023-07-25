adebdc853e909cc8f9a73e78e8e4036798260b22
Vladislav Surkov <vladucha09@inbox.ru>
2023-07-25 23:26:55 +0300

    BookingIntegrationTests.java BookingRepositoryTest.java BookingUnitTests.java ItemRequestUnitTest.java ItemUnitTests.java

--- src/test/java/ru/practicum/shareit/item/service/ItemUnitTests.java
+++ src/test/java/ru/practicum/shareit/item/service/ItemUnitTests.java
@@ -7,7 +7,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import org.springframework.data.domain.PageRequest;
-import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.mapper.BookingMapper;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.model.BookingStatus;

0610ac7e5cb7e53df1264b116e0803665b1ec672
Vladislav Surkov <vladucha09@inbox.ru>
2023-08-02 04:10:03 +0300

    fix : ExceptionsHandler pom.xml pom.xml pom.xml BookingController.java ItemClient.java ItemRequestController.java UserController.java

--- gateway/src/main/java/ru/practicum/shareit/booking/exception/NoBookingException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.booking.exception;
-
-public class NoBookingException extends RuntimeException {
-    public NoBookingException(String message) {
-        super(message);
-    }
-}
--- gateway/src/main/java/ru/practicum/shareit/item/exception/AbsenceException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.item.exception;
-
-public class AbsenceException extends RuntimeException {
-    public AbsenceException(String message) {
-        super(message);
-    }
-}

d0d9f97168b9132805df70f9a693bb3094d96dfe
Vladislav Surkov <vladucha09@inbox.ru>
2023-08-03 21:28:52 +0300

    fix ExceptionsHandler.java, deleted unnecessary files

--- server/src/main/java/ru/practicum/shareit/handler/ExceptionsHandler.java
+++ server/src/main/java/ru/practicum/shareit/handler/ExceptionsHandler.java
@@ -16,7 +16,7 @@ import java.util.Map;
 public class ExceptionsHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleUnhandledException(Exception e) {
-        return new ResponseEntity<>("An unexpected error occurred", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>("An unexpected error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(AbsenceException.class)

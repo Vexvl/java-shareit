5b87b7bdb3566017dc4d62e536ff1e62fb400de5
Vladislav Surkov <vladucha09@inbox.ru>
2023-08-03 19:05:08 +0300

    fix ExceptionsHandler.java, deleted unnecessary files

--- server/src/test/java/ru/practicum/shareit/item/service/ItemIntegrationTests.java
+++ server/src/test/java/ru/practicum/shareit/item/service/ItemIntegrationTests.java
@@ -21,7 +21,6 @@ import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 

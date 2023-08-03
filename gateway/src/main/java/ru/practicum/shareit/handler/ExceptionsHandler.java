592df46ac032129c42ee97a7f9c8998c593332bd
Vladislav Surkov <vladucha09@inbox.ru>
2023-08-03 06:43:36 +0300

    fix ExceptionsHandler.java, deleted unnecessary files

--- gateway/src/main/java/ru/practicum/shareit/handler/ExceptionsHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/handler/ExceptionsHandler.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.handler;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,10 +15,5 @@ public class ExceptionsHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleWrongDateBookingException(WrongDateBookingException e) {
         return Map.of("WrongDateBookingException", e.getMessage());
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleUnhandledException(Exception e) {
-        return new ResponseEntity<>("An unexpected error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
